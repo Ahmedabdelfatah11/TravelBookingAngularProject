65be912d075e98a11306dbed66ae398006cb5b91
Abanoubemad21 <Abanoubemad21>
2025-07-29 00:32:43 +0300

    Add Booking to flight and create reviews with some fixes

--- src/app/Service/flightService.ts
+++ src/app/Service/flightService.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FlightFilterParams, FlightResponse } from '../Models/flight-model';
@@ -6,11 +6,18 @@ import { FlightFilterParams, FlightResponse } from '../Models/flight-model';
 @Injectable({
   providedIn: 'root'
 })
+
 export class FlightService {
   private apiUrl = 'https://localhost:7277/Flight';
   private flightCompanyUrl = 'https://localhost:7277/FlightCompany';
   constructor(private http: HttpClient) { }
-
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('authToken');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': token ? `Bearer ${token}` : ''
+    });
+  }
   getFlightCompany(): Observable<any> {
     return this.http.get<any>(`${this.flightCompanyUrl}`);
   }
@@ -26,6 +33,12 @@ export class FlightService {
   }
   getFlightById(id: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+  bookFlight(flightId: number, seatClass :string): Observable<any> {
+    const body = { seatClass };
+    return this.http.post<any>(`${this.apiUrl}/${flightId}/book`, body, {
+      headers: this.getAuthHeaders()
+    });
   }
 }
 
